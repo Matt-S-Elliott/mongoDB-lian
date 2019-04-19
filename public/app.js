2db74dbbec647033fc537a3996c91518ae1b40d5
Matthew Elliott <mse951993@gmail.com>
2019-04-18 19:40:06 -0700

    added /scrape get on page load

--- public/app.js
+++ public/app.js
@@ -1,11 +1,18 @@
 // Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><br />" + data[i].summary + "<br /><br />" + "<a href='" + data[i].link + "'>Click Here for Article!</a>" + "<br /><hr />" +  "</p>");
-  }
+$.ajax({
+  method: "GET",
+  url: "/scrape"
+}).then(response => {
+  console.log(response);
+  $.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br /><br />" + data[i].summary + "<br /><br />" + "<a href='" + data[i].link + "'>Click Here for Article!</a>" + "<br /><hr />" +  "</p>");
+    }
+  });
 });
+
 
 
 // Whenever someone clicks a p tag
